9ad051f4c823086fa704329fee56528a6688447a
990xan <990xan@gmail.com>
2020-04-13 06:29:13 -0400

    Added tick system + moved chunk gen back to start function

---
+++ Assets/_Content/Scripts/TickSystem/TickManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TickManager : MonoBehaviour
+{
+    public static TickManager current;
+    void Awake(){
+        current = this;
+    }
+
+    void Update(){
+        EnvTick();
+        AtmosTick();
+    }
+
+
+    //Environment ticks
+    //used for everything except world gen and atmos sim
+    [SerializeField]
+    public int envTicks;
+    float envTickTime;
+    private const float envTickTimeMax = .05f;
+
+    public event Action<int> envTickEvent = delegate{};
+
+    void EnvTick(){
+        if (envTickTime >= envTickTimeMax){
+            envTickTime -= envTickTimeMax;
+            envTicks++;
+            
+            envTickEvent(envTicks);
+        }
+        envTickTime += Time.deltaTime;
+    }
+
+    //Atmospherics ticks
+    //used only for atmos sim
+    [SerializeField]
+    public int atmosTicks;
+    float atmosTickTime;
+    private const float atmosTickTimeMax = .2f;
+
+    public event Action<int> atmosTickEvent = delegate{};
+
+    void AtmosTick(){
+        if (atmosTickTime >= atmosTickTimeMax){
+            atmosTickTime -= atmosTickTimeMax;
+            atmosTicks++;
+            atmosTickEvent(atmosTicks);
+        }
+        atmosTickTime += Time.deltaTime;
+    }
+
+}
